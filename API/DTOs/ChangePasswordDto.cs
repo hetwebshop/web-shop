edf41b57d95662349d5385dc412f582f84b11c90
emin1234 <emindukic123@gmail.com>
2025-04-22 22:21:43 +0200

    Final fixes before PRODUCTION RELEASE

--- API/DTOs/ChangePasswordDto.cs
+++ API/DTOs/ChangePasswordDto.cs
@@ -11,7 +11,7 @@ namespace API.DTOs
         public string CurrentPassword { get; set; }
         [Required(ErrorMessage = "Lozinka je obavezna.")]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Lozinka mora imati između 8 i 100 karaktera.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\W]{8,}$",
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\+\-=\[\]{}|:;""'<>,\.?/])[A-Za-z\d!@#$%^&*()_\+\-=\[\]{}|:;""'<>,\.?/]{8,}$",
             ErrorMessage = "Lozinka mora sadržavati najmanje jedno veliko slovo, jedno malo slovo, jedan broj i jedan specijalni znak.")]
         public string NewPassword { get; set; }
         [Required(ErrorMessage = "Potvrda lozinke je obavezna.")]
--- API/DTOs/UserRegisterDto.cs
+++ API/DTOs/UserRegisterDto.cs
@@ -17,7 +17,7 @@ namespace API.DTOs
 
         [Required(ErrorMessage = "Lozinka je obavezna.")]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Lozinka mora imati između 8 i 100 karaktera.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\W]{8,}$",
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\+\-=\[\]{}|:;""'<>,\.?/])[A-Za-z\d!@#$%^&*()_\+\-=\[\]{}|:;""'<>,\.?/]{8,}$",
             ErrorMessage = "Lozinka mora sadržavati najmanje jedno veliko slovo, jedno malo slovo, jedan broj i jedan specijalni znak.")]
         public string Password { get; set; }
 
