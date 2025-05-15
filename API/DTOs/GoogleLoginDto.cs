db31d3de3d75b243a5b9b9ac14ca05af8dd514be
emin1234 <emindukic123@gmail.com>
2025-05-15 20:14:00 +0200

    Google Login

---
+++ API/DTOs/GoogleLoginDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class GoogleLoginDto
+    {
+        public string IdToken { get; set; }
+        public string IPAddress { get; set; }
+        public string UserAgent { get; set; }
+        public string DeviceId { get; set; }
+    }
+}
